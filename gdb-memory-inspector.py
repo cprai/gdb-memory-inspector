49e53b8e70a4eab0edb59355f411025b8f05e12a
Chuck Rai <cprai@sfu.ca>
2019-12-28 18:48:54 -0800

    Use PyQt instead of kivy

--- gdb-memory-inspector.py
+++ gdb-memory-inspector.py
@@ -1,22 +1,65 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
+import json
+import threading
+from PyQt5.QtWidgets import *
+from pygdbmi import gdbmiparser
+from pygdbmi import gdbcontroller
 
-class LoginScreen(GridLayout):
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+class MemorySpaceView(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(QLabel("Hello Qt"))
+        btn = QPushButton('Top')
+        #btn.clicked.connect(self.parent.fn)
+        self.layout.addWidget(btn)
+        self.layout.addWidget(QPushButton('Bottom'))
+        self.table = QTableWidget(0, 3)
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.layout.addWidget(self.table)
+        self.setLayout(self.layout)
+    
+    def library_loaded(self, library):
+        self.table.insertRow(self.table.rowCount())
+        self.table.setItem(self.table.rowCount() - 1, 0, QTableWidgetItem(library["id"]))
+        self.table.setItem(self.table.rowCount() - 1, 1, QTableWidgetItem(library["ranges"][0]["from"]))
+        self.table.setItem(self.table.rowCount() - 1, 2, QTableWidgetItem(library["ranges"][0]["to"]))
 
-class MainWindow(App):
-    def build(self):
-        return LoginScreen()
+class GdbMemoryInspector(QApplication):
+    def __init__(self):
+        super().__init__([])
+
+        # Start gdb
+        self.gdbmi = gdbcontroller.GdbController()
+        self.gdbmi_read_thread = threading.Thread(target=self.gdbmi_read_thread)
+        self.gdbmi_read_thread.start()
+
+        # Start program
+        self.gdbmi_execute("-target-attach 15108")
+
+        # Init UI
+        self.create_interface()
+    
+    def gdbmi_read_thread(self):
+        try:
+            responses = self.gdbmi.get_gdb_response()
+        except gdbcontroller.GdbTimeoutError:
+            pass
+        else:
+            for response in responses:
+                self.gdbmi_handle_response(response)
+    
+    def gdbmi_handle_response(self, response):
+        print(json.dumps(response, indent=2))
+        if response["type"] == "notify" and response["message"] == "library-loaded":
+            self.window.library_loaded(response["payload"])
+
+    def gdbmi_execute(self, command):
+        self.gdbmi.write(command, read_response=False)
+
+    def create_interface(self):
+        self.window = MemorySpaceView(self)
+        self.window.show()
 
 if __name__ == "__main__":
-    MainWindow().run()
+    GdbMemoryInspector().exec_()
